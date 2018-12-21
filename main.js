21ffd940d442b280cffac96cdec1df4ee33ce4dd
Nabil Fannane <mzabi30@gmail.com>
2018-12-21 14:27:12 -0800

    update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btn=document.querySelector('.nabbtnt');
 const nav=document.querySelector('.nav');
-
+const video_switch=document.querySelector('.video_switch_container');
+const btn_video=document.querySelector('.video_switch-btn')
 
 window.addEventListener('load',()=>
  
@@ -8,22 +9,30 @@ setTimeout(()=>{
    document.querySelector('.preloader').style.display="none";
   
 
-},2000)
-
-       
-   )
-
-
+},2000) )
 
 
 btn.addEventListener('click',()=>{
  
     nav.classList.toggle('hide-nav');
    
-  
-    
 })
 
+
+
+video_switch.addEventListener('click',()=>{
+
+    if(!btn_video.classList.contains('btn_slide')){
+
+        btn_video.classList.add('btn_slide');
+        document.querySelector('.video_item').pause();
+    }
+    else{
+        btn_video.classList.remove('btn_slide');
+        document.querySelector('.video_item').play();
+   
+    }
+})
 
 
 
