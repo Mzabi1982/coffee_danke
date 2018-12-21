8fc4ca7b75ad73c9ae2b848f96bc54d45a677404
Nabil Fannane <mzabi30@gmail.com>
2018-12-21 13:25:58 -0800

    update

---
+++ main.js
@@ -0,0 +1,32 @@
+const btn=document.querySelector('.nabbtnt');
+const nav=document.querySelector('.nav');
+
+
+window.addEventListener('load',()=>
+ 
+setTimeout(()=>{
+   document.querySelector('.preloader').style.display="none";
+  
+
+},2000)
+
+       
+   )
+
+
+
+
+btn.addEventListener('click',()=>{
+ 
+    nav.classList.toggle('hide-nav');
+   
+  
+    
+})
+
+
+
+
+
+ 
+
