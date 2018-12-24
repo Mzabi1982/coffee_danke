d8a0ae6f46c01e3c9a598b2a6ad14431973604b0
Nabil Fannane <mzabi30@gmail.com>
2018-12-23 21:20:05 -0800

    test

--- main.js
+++ main.js
@@ -66,9 +66,96 @@ video_switch.addEventListener('click',()=>{
 })
 
 
+function Customer(fname,lname,email){
+    this.fname=fname,
+    this.lname=lname,
+    this.email=email
+}
+
+ function addt(person){
+
+     console.log(person)
+    const images=[1,2,3,4,5]
+    let random = Math.floor(random()*images.length);
+    const div=document.createElement('div');
+    div.classList.add('person');
+    div.innerHTML=`<img src="./medias/free${random}.jpg" height="50px" width="50px" h alt="" class="person">
+    <h4 class="person-fname">${person.fname}</h4>
+    <h4 class="person-lname">${person.lname}</h4>`
+
+    document.querySelector('.card-list').appendChild(div);
+   
+ }
+
+
+const form=document.querySelector('.drink-form');
+form.addEventListener('submit',(e)=>{
+    const fname=document.querySelector('.fname').value;
+    const lname=document.querySelector('.lname').value;
+    const email=document.querySelector('.email').value;
+
+     if (check(fname,lname,email)){
+         let customer =new Customer(fname,lname,email);
+        // addt(customer);
+        feedback('Customer succefully added','success')   
+     }else{
+        feedback('Some or all fields are empty','error') 
+     }
+
+
+    
+
+ e.preventDefault();
+    
+})
+
+
+
+
+
+
+
+const check=(f,l,e)=>{
+ let result;
+  if(f==='' || l==='' || e==='')
+  result=false;
+  else result=true
+
+  return result;
+}
+
+feedback=(text,type)=>{
+  if (type==='success'){
+
+    let feedback=document.querySelector('.drink-feedback')
+    feedback.classList.add('success');
+    feedback.innerHTML=text;
+    removealert('success');
+  
+    
+  }
+  else if (type==='error'){
+      let feedback=document.querySelector('.drink-feedback')
+      feedback.classList.add('error');
+      feedback.innerHTML=text;
+      removealert('error')
+  }
+}
+
+const removealert=(value)=>{
+
+    setTimeout(()=>{
+        document.querySelector('.drink-feedback').classList.remove(value);
+
+    },1500)
+}
+
+
+
+
+
+
 window.sr = ScrollReveal();
-
-
 
 sr.reveal('.banner_title', {
     duration: 4000,
